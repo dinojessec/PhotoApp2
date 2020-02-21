5d6c9236716aeb70150ec68913d0b652eb550eac
Paul <pcmprieto@gmail.com>
2020-02-21 11:46:36 +0800

    DotApp Enabling Camera function and permissions

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -27,8 +28,24 @@ import {
 
 import {RNPhotoEditor} from 'react-native-photo-editor';
 import RNFS from 'react-native-fs';
+import { RNCamera } from 'react-native-camera';
+
+
+const PendingView = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'lightgreen',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Text>Waiting</Text>
+  </View>
+);
 
 const App: () => React$Node = () => {
+  const util = require('util')
   _onPress = () => {
     RNPhotoEditor.Edit({
       path: RNFS.DocumentDirectoryPath + '/photo.jpg',
@@ -57,16 +74,48 @@ const App: () => React$Node = () => {
       colors: undefined,
       onDone: () => {
         console.log('on done');
+        console.log(util.inspect(RNCamera, false, null, true /* enable colors */))
       },
       onCancel: () => {
         console.log('on cancel');
+
       },
     });
   };
 
+  takePicture = async function(camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+    //  eslint-disable-next-line
+    console.log(data.uri);
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={this._onPress}>
+      <RNCamera 
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        
+      >
+        {({ camera, status }) => {
+          if (status !== 'READY') return <PendingView />;
+          return (
+            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+              <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
+                <Text style={{ fontSize: 14 }}> SNAP </Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      </RNCamera>
+      <TouchableOpacity onPress={_onPress}>
         <Text>Click</Text>
       </TouchableOpacity>
     </View>
@@ -74,8 +123,18 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
   scrollView: {
     backgroundColor: Colors.lighter,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   engine: {
     position: 'absolute',
